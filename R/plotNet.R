45fb9002e80aa996104ceaea28ee827a73bf3ea8
Zach <zdkurtz@gmail.com>
2014-05-01 01:51:23 -0400

    fixed export statement

--- R/plotNet.R
+++ R/plotNet.R
@@ -1,6 +1,7 @@
 #' Convert an adjacency matrix (ie - from the \code{sparseiCov} function) to an igraph object suitable for plotting via phyloseq's \code{plot_network}.
 #' 
 #' @param Adj an Adjacency matrix
+#' @export
 adj2igraph <- function(Adj, names=1:ncol(Adj), rmEmptyNodes=FALSE) {
     g <- graph.adjacency(Adj)
 
