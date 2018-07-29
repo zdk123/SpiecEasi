00046292fe9647e4e223df8bd16abe49bb8a0587
Zachary Kurtz <zkurtz@lodotherapeutics.com>
2018-07-29 15:32:39 -0400

    remove writes in tests

--- tests/testthat/test_pulsar.R
+++ tests/testthat/test_pulsar.R
@@ -29,8 +29,8 @@ bout.stars <- spiec.easi(X, method='mb', verbose=FALSE, lambda.max=lmx, lambda.m
 bout.bstars <- spiec.easi(X, method='mb', verbose=FALSE, lambda.max=lmx, lambda.min.ratio=lmr, nlambda=nlam, sel.criterion='bstars', pulsar.select='batch', pulsar.params=pargs)
 
 
-save(out, out.stars, out.bstars, bout.stars, bout.bstars,
-    file=system.file('testdata', 'sepulsar.RData', package='SpiecEasi'))
+# save(out, out.stars, out.bstars, bout.stars, bout.bstars,
+#     file=system.file('testdata', 'sepulsar.RData', package='SpiecEasi'))
 
 test_that("no pulsar has same output", {
   expect_equal(as.matrix(out$est$path[[out.stars$select$stars$opt.index]]),
@@ -61,3 +61,44 @@ test_that("stars == batch bstars", {
 
 
 ## TODO: test pulsar params in/out
+
+
+context('spiec.easi getters')
+
+# load(system.file('testdata', 'sepulsar.RData', package='SpiecEasi'))
+
+test_that("Getter API throws errors if no pulsar selection", {
+  expect_error(getOptInd(out))
+  expect_error(getOptNet(out))
+  expect_error(getRefit(out))
+  expect_error(getOptLambda(out))
+  expect_error(getOptMerge(out))
+  expect_error(getOptCov(out))
+  expect_error(getOptiCov(out))
+  expect_error(getOptBeta(out))
+})
+
+runtests <- function(out) {
+  expect_equal(getOptInd(out), (i<-out$select$stars$opt.index))
+  expect_equal(getOptNet(out), out$refit$stars)
+  expect_equal(getRefit(out), out$refit$stars)
+  expect_equal(getOptLambda(out), out$lambda[i])
+  expect_equal(getOptMerge(out), out$select$stars$merge[[i]])
+  expect_equal(getOptBeta(out), out$est$beta[[i]])
+}
+
+test_that("Getter API, pulsar / stars ", {
+  runtests(out.stars)
+})
+
+test_that("Getter API, pulsar / bstars ", {
+  runtests(out.bstars)
+})
+
+test_that("Getter API, batch pulsar / stars ", {
+  runtests(bout.stars)
+})
+
+test_that("Getter API, batch pulsar / bstars ", {
+  runtests(bout.bstars)
+})
